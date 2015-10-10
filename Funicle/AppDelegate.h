ebe11ffd027b7d6817c03ebd3dc764de6adfa7b5
Sebastian Dobrincu <sebyddd@gmail.com>
2015-10-10 15:39:17 +0300

    Initial Commit

---
+++ Funicle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Funicle
+//
+//  Created by Sebastian Dobrincu on 10/10/15.
+//  Copyright © 2015 Sebastian Dobrincu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Funicle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Funicle
+//
+//  Created by Sebastian Dobrincu on 10/10/15.
+//  Copyright © 2015 Sebastian Dobrincu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
